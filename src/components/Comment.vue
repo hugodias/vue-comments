9e1b6f5e0e6761e8daa71ca1b81bc220ca8f2bff
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2017-04-14 15:56:39 -0300

    init

---
+++ src/components/Comment.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="comment">
+    <div class="avatar">
+      <img src="https://placehold.it/150x150">
+    </div>
+    <div class="comment-body">
+      <h4>{{ username }}</h4>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comment',
+  props: ['text', 'username'],
+};
+</script>
+
+<style scoped>
+* {
+  text-align: left;
+}
+.comment {
+  float: left;
+  width: 500px;
+  border: 1px solid #EEE;
+  padding: 15px;
+}
+.avatar {
+  float: left;
+  width: 70px;
+}
+
+.comment-body {
+  float: left;
+  width: 430px;
+}
+.avatar img {
+  width: 50px;
+  height: 50px;
+}
+h4 {
+  margin: 0;
+}
+p {
+  margin: 0;
+  font-size: 14px;
+  float: left;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <div class="comments">
+      <input v-model="message" v-on:keyup.enter="saveMessage" placeholder="Comment here">
+      <button v-on:click="saveMessage">Comment</button>
+      <hr/>
+      <comment
+         v-for="comment in comments"
+         v-bind:key="comment"
+         v-bind:text="comment.text"
+         v-bind:username="comment.username"></comment>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Comment from '@/components/Comment';
+
+export default {
+  name: 'hello',
+  methods: {
+    saveMessage(event) {
+      console.log(event);
+      console.log(this.message);
+
+      this.comments.push({
+        id: this.comments.lenght + 1,
+        text: this.message,
+        username: 'Hugo Dias',
+      });
+
+      this.message = '';
+    },
+  },
+  data() {
+    return {
+      message: '',
+      msg: 'Latest comments',
+      comments: [
+        {
+          id: 1,
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Post enim Chrysippum eum non sane est disputatum.',
+          username: 'Hugo Dias',
+        },
+      ],
+    };
+  },
+  components: {
+    Comment,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.comments {
+  width: 530px;
+  margin: 0 auto;
+}
+
+input {
+  float: left;
+  width: 420px;
+  margin-bottom: 20px;
+  height: 30px;
+  padding-left: 10px;
+  border: 1px solid #EEE;
+}
+
+input:hover,
+input:focus {
+  outline: none;
+}
+
+button {
+  float: right;
+  background-color: #42b983;
+  color: #FFF;
+  border: 1px solid #35495E;
+  padding: 10px 15px;
+  font-weight: bold;
+}
+hr {
+  clear: both;
+  border: 1px solid #EEE;
+  margin-bottom: 20px;
+}
+</style>
